9c7b9886b52357dbed50147a9500124775a9e03a
callmecavs <callmecavs@gmail.com>
2016-05-14 02:51:06 -0400

    add end function (proto)

--- src/jump.js
+++ src/jump.js
@@ -1,6 +1,6 @@
 import easeInOutQuad from './easing'
 
-export default ({
+const jumper = ({
 
   // defaults
   offset = 0,
@@ -44,7 +44,16 @@ export default ({
   }
 
   function end() {
+    // account for rounding inaccuracies
+    window.scrollTo(0, start + distance)
 
+    // if it exists, fire the callback
+    if(typeof callback === 'function') {
+      callback()
+    }
+
+    // reset for next jump
+    timeStart = undefined
   }
 
   function jump(target, options = {}) {
@@ -92,52 +101,3 @@ export default ({
     jump
   }
 }
-
-// export default class Jump {
-//   jump(target, options = {}) {
-//     // this.start = window.pageYOffset
-//
-//     this.options = {
-//       duration: options.duration,
-//       offset: options.offset || 0,
-//       callback: options.callback,
-//       easing: options.easing || easeInOutQuad
-//     }
-//
-//     this.target = typeof target === 'string'
-//       ? document.querySelector(target)
-//       : target;
-//
-//     this.distance = typeof target === 'number'
-//       ? target
-//       : this.options.offset + target.getBoundingClientRect().top;
-//
-//     this.duration = typeof this.options.duration === 'function'
-//       ? this.options.duration(this.distance)
-//       : this.options.duration
-//
-//     requestAnimationFrame(time => this._loop(time))
-//   }
-//
-//   _loop(time) {
-//     if(!this.timeStart) {
-//       this.timeStart = time
-//     }
-//
-//     this.timeElapsed = time - this.timeStart
-//     this.next = this.options.easing(this.timeElapsed, this.start, this.distance, this.duration)
-//
-//     window.scrollTo(0, this.next)
-//
-//     this.timeElapsed < this.duration
-//       ? requestAnimationFrame(time => this._loop(time))
-//       : this._end()
-//   }
-//
-//   _end() {
-//     window.scrollTo(0, this.start + this.distance)
-//
-//     typeof this.options.callback === 'function' && this.options.callback()
-//     this.timeStart = false
-//   }
-// }
