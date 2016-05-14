18230ff63defe6c80766fc4bab61d0a9f706cea2
callmecavs <callmecavs@gmail.com>
2016-05-14 01:51:53 -0400

    start rewrite

--- src/jump.js
+++ src/jump.js
@@ -1,50 +1,92 @@
 import easeInOutQuad from './easing'
 
-export default class Jump {
-  jump(target, options = {}) {
-    this.start = window.pageYOffset
+export default defaults = ({
 
-    this.options = {
-      duration: options.duration,
-      offset: options.offset || 0,
-      callback: options.callback,
-      easing: options.easing || easeInOutQuad
-    }
+  // defaults
+  offset = 0,
+  easing = easeInOutQuad
 
-    this.target = typeof target === 'string'
-      ? document.querySelector(target)
-      : target;
+}) => {
 
-    this.distance = typeof target === 'number'
+  // globals
+  let start
+  let duration
+
+  function loop() {
+
+  }
+
+  function jump(target, options = {}) {
+    // cache starting position
+    start = window.scrollY || window.pageYOffset
+
+    // cache duration
+    // the only required option
+    duration = options.duration
+
+    // resolve target
+    // if not an element, assume its a selector
+    const to = target.nodeType
       ? target
-      : this.options.offset + target.getBoundingClientRect().top;
+      : document.querySelector(target)
 
-    this.duration = typeof this.options.duration === 'function'
-      ? this.options.duration(this.distance)
-      : this.options.duration
 
-    requestAnimationFrame(time => this._loop(time))
   }
 
-  _loop(time) {
-    if(!this.timeStart) {
-      this.timeStart = time
-    }
+  function end() {
 
-    this.timeElapsed = time - this.timeStart
-    this.next = this.options.easing(this.timeElapsed, this.start, this.distance, this.duration)
-
-    window.scrollTo(0, this.next)
-
-    this.timeElapsed < this.duration
-      ? requestAnimationFrame(time => this._loop(time))
-      : this._end()
   }
 
-  _end() {
-    window.scrollTo(0, this.start + this.distance)
-
-    typeof this.options.callback === 'function' && this.options.callback()
-    this.timeStart = false
+  return {
+    jump
   }
 }
+
+// export default class Jump {
+//   jump(target, options = {}) {
+//     // this.start = window.pageYOffset
+//
+//     this.options = {
+//       duration: options.duration,
+//       offset: options.offset || 0,
+//       callback: options.callback,
+//       easing: options.easing || easeInOutQuad
+//     }
+//
+//     this.target = typeof target === 'string'
+//       ? document.querySelector(target)
+//       : target;
+//
+//     this.distance = typeof target === 'number'
+//       ? target
+//       : this.options.offset + target.getBoundingClientRect().top;
+//
+//     this.duration = typeof this.options.duration === 'function'
+//       ? this.options.duration(this.distance)
+//       : this.options.duration
+//
+//     requestAnimationFrame(time => this._loop(time))
+//   }
+//
+//   _loop(time) {
+//     if(!this.timeStart) {
+//       this.timeStart = time
+//     }
+//
+//     this.timeElapsed = time - this.timeStart
+//     this.next = this.options.easing(this.timeElapsed, this.start, this.distance, this.duration)
+//
+//     window.scrollTo(0, this.next)
+//
+//     this.timeElapsed < this.duration
+//       ? requestAnimationFrame(time => this._loop(time))
+//       : this._end()
+//   }
+//
+//   _end() {
+//     window.scrollTo(0, this.start + this.distance)
+//
+//     typeof this.options.callback === 'function' && this.options.callback()
+//     this.timeStart = false
+//   }
+// }
