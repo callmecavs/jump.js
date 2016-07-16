8cbb3f872362cdbbf53a0d7272c17e3f4d22f485
Michael Cavalea <callmecavs@gmail.com>
2016-07-16 01:32:14 -0400

    update easing.js comments

--- src/easing.js
+++ src/easing.js
@@ -1,4 +1,7 @@
-// Robert Penner's easeInOutQuad - http://robertpenner.com/easing/
+// Robert Penner's easeInOutQuad
+
+// find the rest of his easing functions here: http://robertpenner.com/easing/
+// find them exported for ES6 consumption here: https://github.com/jaxgeller/ez.js
 
 export default (t, b, c, d) => {
   t /= d / 2
