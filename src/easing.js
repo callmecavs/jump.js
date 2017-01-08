8aab6239d564a69eccbb99712956418f9a7b43a3
callmecavs <callmecavs@gmail.com>
2017-01-08 16:38:14 -0500

    fix linting errors

--- src/easing.js
+++ src/easing.js
@@ -5,7 +5,7 @@
 
 const easeInOutQuad = (t, b, c, d) => {
   t /= d / 2
-  if(t < 1) return c / 2 * t * t + b
+  if (t < 1) return c / 2 * t * t + b
   t--
   return -c / 2 * (t * (t - 2) - 1) + b
 }
