ee2fd5a7410dc8d910c034173d781e573c24eae8
Michael Cavalea <callmecavs@gmail.com>
2026-06-12 17:23:19 -0400

    need to run a dev server runs for the tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="node" />
+
 import { defineConfig, devices } from "@playwright/test"
 
 /**
@@ -26,7 +28,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: "http://localhost:3000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -71,9 +73,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "pnpm run test:server",
+    url: "http://localhost:3000",
+    reuseExistingServer: !process.env.CI,
+  },
 })
