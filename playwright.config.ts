94c3de90e351bf83c5c10f5f8b36119d73734820
Michael Cavalea <callmecavs@gmail.com>
2026-06-13 08:21:03 -0400

    cleanup `pnpm` calls

--- playwright.config.ts
+++ playwright.config.ts
@@ -74,7 +74,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "pnpm run test:server",
+    command: "pnpm test:server",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
   },
