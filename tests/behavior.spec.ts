8895464bfc9e797e4b3965fc9c8eb102e73652b2
Michael Cavalea <callmecavs@gmail.com>
2026-06-14 10:26:58 -0400

    add `window.scrollTo` capturing to fixtures

--- tests/behavior.spec.ts
+++ tests/behavior.spec.ts
@@ -5,11 +5,14 @@ import type { Jump, JumpOptions, JumpTarget } from "../src/types"
 declare global {
   interface Window {
     fixtures: {
-      getY: () => number
+      captureWindowScrollToArgs: () => void
+      getLatestWindowScrollToArgs: () => number
+
+      getWindowY: () => number
+      setWindowY: (top: number) => void
+
       jump: Jump
-      reset: () => void
       scroll: (target: JumpTarget, options?: JumpOptions) => Promise<void>
-      setY: (top: number) => void
     }
   }
 }
@@ -20,13 +23,13 @@ test.beforeEach(async ({ page }) => {
 })
 
 test("target: number", async ({ page }) => {
-  await page.evaluate(async () => window.fixtures.setY(50))
+  await page.evaluate(async () => window.fixtures.setWindowY(50))
   await page.evaluate(async () => window.fixtures.scroll(100))
-  expect(await page.evaluate(async () => window.fixtures.getY())).toEqual(150)
+  expect(await page.evaluate(async () => window.fixtures.getWindowY())).toEqual(150)
 })
 
 test("target: number ignores offset", async ({ page }) => {
-  await page.evaluate(async () => window.fixtures.setY(50))
+  await page.evaluate(async () => window.fixtures.setWindowY(50))
   await page.evaluate(async () => window.fixtures.scroll(100, { offset: -50 }))
-  expect(await page.evaluate(async () => window.fixtures.getY())).toEqual(150)
+  expect(await page.evaluate(async () => window.fixtures.getWindowY())).toEqual(150)
 })
