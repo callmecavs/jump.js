118aebaaf8644c1e35ce4f07a8b324cf6cabc0ca
Michael Cavalea <callmecavs@gmail.com>
2026-06-14 10:39:16 -0400

    cleanup unneeded `async`

--- tests/behavior.spec.ts
+++ tests/behavior.spec.ts
@@ -23,20 +23,20 @@ test.beforeEach(async ({ page }) => {
 })
 
 test("target: number", async ({ page }) => {
-  await page.evaluate(async () => window.fixtures.setWindowY(50))
-  await page.evaluate(async () => window.fixtures.scroll(100))
-  expect(await page.evaluate(async () => window.fixtures.getWindowY())).toEqual(150)
+  await page.evaluate(() => window.fixtures.setWindowY(50))
+  await page.evaluate(() => window.fixtures.scroll(100))
+  expect(await page.evaluate(() => window.fixtures.getWindowY())).toEqual(150)
 })
 
 test("target: number ignores offset", async ({ page }) => {
-  await page.evaluate(async () => window.fixtures.setWindowY(50))
-  await page.evaluate(async () => window.fixtures.scroll(100, { offset: -50 }))
-  expect(await page.evaluate(async () => window.fixtures.getWindowY())).toEqual(150)
+  await page.evaluate(() => window.fixtures.setWindowY(50))
+  await page.evaluate(() => window.fixtures.scroll(100, { offset: -50 }))
+  expect(await page.evaluate(() => window.fixtures.getWindowY())).toEqual(150)
 })
 
 test("target: number is clamped (start)", async ({ page }) => {
-  await page.evaluate(async () => window.fixtures.setWindowY(25))
-  await page.evaluate(async () => window.fixtures.captureWindowScrollToArgs())
-  await page.evaluate(async () => window.fixtures.scroll(-50))
-  expect(await page.evaluate(async () => window.fixtures.getLatestWindowScrollToArgs())).toEqual([{ top: 0 }])
+  await page.evaluate(() => window.fixtures.setWindowY(25))
+  await page.evaluate(() => window.fixtures.captureWindowScrollToArgs())
+  await page.evaluate(() => window.fixtures.scroll(-50))
+  expect(await page.evaluate(() => window.fixtures.getLatestWindowScrollToArgs())).toEqual([{ top: 0 }])
 })
