37928e4326ed3bff949f51b726d65c5dc7e56ef8
Michael Cavalea <callmecavs@gmail.com>
2026-08-03 18:46:10 -0400

    finish examples

---
+++ examples/cancel-on-user-input.ts
@@ -0,0 +1,67 @@
+import type { JumpCancel, JumpOptions, JumpTarget } from "jump.js"
+import scroll from "jump.js" // NOTE: import renamed, reserving `jump` for the export
+
+const SCROLL_KEYS = [
+  " ", // Spacebar
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowUp",
+  "End",
+  "Home",
+  "PageDown",
+  "PageUp",
+  "Tab", // focus change can cause scrolling
+]
+
+const jump = (target: JumpTarget, options: JumpOptions = {}): JumpCancel => {
+  let cancel: JumpCancel
+
+  const handleCancel = () => {
+    cleanup()
+    cancel()
+  }
+
+  // keyboard input
+  const handleKeyDown = ({ key }: KeyboardEvent) => {
+    if (SCROLL_KEYS.includes(key)) handleCancel()
+  }
+
+  // touch or pen input
+  const handlePointerDown = ({ pointerType }: PointerEvent) => {
+    if (pointerType !== "mouse") handleCancel()
+  }
+
+  // mouse wheel or trackpad input
+  const handleWheel = () => handleCancel()
+
+  const setup = () => {
+    window.addEventListener("keydown", handleKeyDown)
+    window.addEventListener("pointerdown", handlePointerDown, { passive: true })
+    window.addEventListener("wheel", handleWheel, { passive: true })
+  }
+
+  const cleanup = () => {
+    window.removeEventListener("keydown", handleKeyDown)
+    window.removeEventListener("pointerdown", handlePointerDown)
+    window.removeEventListener("wheel", handleWheel)
+  }
+
+  const mergedCallback = () => {
+    cleanup()
+    options.callback?.()
+  }
+
+  try {
+    cancel = scroll(target, { ...options, callback: mergedCallback })
+
+    setup()
+
+    return handleCancel
+  } catch (error) {
+    cleanup()
+    throw error
+  }
+}
+
+export { jump }
--- examples/prefers-reduced-motion.ts
+++ examples/prefers-reduced-motion.ts
@@ -1,9 +1,9 @@
 import jump from "jump.js"
 
-// detect user's motion preference
+// detect motion preference
 const isReducedMotion = window.matchMedia("(prefers-reduced-motion: reduce)").matches
 
-// set `duration` option based on preference
+// set `duration` based on preference
 const duration = isReducedMotion ? 0 : 1000
 
 jump(".target", { duration })
