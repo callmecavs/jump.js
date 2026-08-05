7c8df3d8a295fd1d4987c31459c52f983a524bf3
Michael Cavalea <callmecavs@gmail.com>
2026-08-05 19:42:15 -0400

    rm note re: browser support from recipe

--- recipes/prefers-reduced-motion.ts
+++ recipes/prefers-reduced-motion.ts
@@ -1,6 +1,5 @@
 import jump from "jump.js"
 
-// NOTE: The `prefers-reduced-motion` media query doesn't match the "Browser Support" shown on the README.
 // detect motion preference
 const isReducedMotion = window.matchMedia("(prefers-reduced-motion: reduce)").matches
 
